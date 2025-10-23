42e43b2223c495aeac3fad8182f7343a63aaea45
Fernanda Nohemi Gomez Garcia <fernanda.gomez070206@gmail.com>
2025-10-22 22:08:36 -0600

    shiny

--- api.js
+++ api.js
@@ -4,16 +4,18 @@ const id = document.getElementById("id");
 const imagen = document.getElementById("imagen");
 const sonido = document.getElementById("sonido");
 const listaHabilidades = document.getElementById("listaHabilidades");
+const shiny = document.getElementById("shiny");
 
 async function buscarPokemon(){
     const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/" + inputnombre.value);
     const infoPokemon = await respuesta.json();
-    
+
     //poner el id en el parerafo llamado id
     id.innerText = infoPokemon.id;
 
     //mostrar iumagen
     imagen.src = infoPokemon.sprites.other.home.front_default;
+    shiny.src = infoPokemon.sprites.front_shiny || infoPokemon.sprites.front_default;
 
     //Mostrar audio
     sonido.src = infoPokemon.cries.latest;
