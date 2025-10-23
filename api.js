3b32ff9c151af24c3252833ef96324d5abf27490
Fernanda Nohemi Gomez Garcia <fernanda.gomez070206@gmail.com>
2025-10-22 19:52:50 -0600

    estilos

---
+++ api.js
@@ -0,0 +1,36 @@
+const botonBuscar = document.getElementById("botonBuscar");
+const inputnombre = document.getElementById("nombre");
+const id = document.getElementById("id");
+const imagen = document.getElementById("imagen");
+const sonido = document.getElementById("sonido");
+const listaHabilidades = document.getElementById("listaHabilidades");
+
+async function buscarPokemon(){
+    const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/" + inputnombre.value);
+    const infoPokemon = await respuesta.json();
+        //poner el id en el parerafo llamado id
+    id.innerText = infoPokemon.id;
+    //poner el id en el parerafo llamado id
+    id.innerText = infoPokemon.id;
+
+    //mostrar iumagen
+    imagen.src = infoPokemon.sprites.other.home.front_default;
+
+    //Mostrar audio
+    sonido.src = infoPokemon.cries.latest;
+
+    //Mostrar habiliades
+    listaHabilidades.innerHTML = "";
+
+    infoPokemon.abilities.foreach(a => {
+        const item = document.createElement("li")
+        item.innerText = a.ability.name;
+        listaHabilidades.appendChild(item);
+    });
+};
+botonBuscar.addEventListener("click", e => {
+    e.preventDefault();
+    console.log("hola");
+    buscarPokemon();
+
+})
